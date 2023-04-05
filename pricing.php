800243874ee1ecc6b532abc64f2d2230a27fde9a
AndyVSP <andrea.salazarparra@mail.mcgill.ca>
2023-04-05 00:32:16 -0400

    update

--- pricing.php
+++ pricing.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Renata Salazar</title>
+        <title>Renata Salazar - Pricing</title>
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Caveat+Brush&family=Lustria&family=Montserrat&display=swap" rel="stylesheet">
@@ -13,86 +13,119 @@
     </head>
     <body>
         <?php include('header.php'); ?>
-        <div class="container-fluid d-flex">
-            <div class="col-4">
-                <section class ="header-section"> 
-                    <h1>Pricing</h1>
-                </section>
-                <section id = "commissions" class ="subHeading-section">
-                        <h4> Commissions </h4>
-                </section>
-                <div class = "scroll body-section">
-                    <ul id = "selectedWorks">
-                         <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                        <li>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                        </li>
-                    </ul>
-                </div>
-                <section id = "faq" class ="subHeading-section"> 
-                 <h4> FAQs </h4>
-                </seciton>
-                <div class="accordion accordion-flush-accordion" id="pricingInfo">
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
-                            Payment Methods
-                        </button>
-                        </h2>
-                        <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                            <ul id = "paymentFAQ">
-                                <li>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </li>
-                                <li>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </li>
-                                <li>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </li>
-                            </ul>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-4">
+                    <section class ="header-section"> 
+                        <h1>Pricing</h1>
+                    </section>
+                    <section id = "commissions" class ="subHeading-section">
+                            <h4> Commissions </h4>
+                    </section>
+                    <div class = "scroll body-section">
+                        <ul id = "selectedWorks">
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                            <li>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                            </li>
+                        </ul>
+                    </div>
+                    <section id = "faq" class ="subHeading-section"> 
+                    <h4> FAQs </h4>
+                    </seciton>
+                    <div class="accordion accordion-flush-accordion" id="pricingInfo">
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed customAccordionButton" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+                                    Payment Methods
+                                </button>
+                            </h2>
+                            <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">
+                                    <ul id = "paymentFAQ">
+                                    <li>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </li>
+                                    <li>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </li>
+                                    <li>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                    </li>
+                                    </ul>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header">
+                                <button class="accordion-button collapsed customAccordionButton" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
+                                    Copyright
+                                </button>
+                            </h2>
+                            <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
+                                <div class="accordion-body">
+                                    <ul id = "copyrightFAQ">
+                                        <li>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        </li>
+                                        <li>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        </li>
+                                        <li>
+                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                                        </li>
+                                    </ul>
+                                </div>
+                            </div>
                         </div>
                     </div>
-                    <div class="accordion-item">
-                        <h2 class="accordion-header">
-                        <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-                            Copyright
-                        </button>
-                        </h2>
-                        <div id="flush-collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample">
-                        <div class="accordion-body">
-                        <ul id = "copyrightFAQ">
-                                <li>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </li>
-                                <li>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </li>
-                                <li>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                                </li>
-                            </ul>
+                </div>
+                <div id = "sampleImages" class="col-4">
+                    <div class="card pricing_imgTop" style="height: 15rem; width: 18rem;">
+                        <img src="Assets/img/PricingSample_1.png"  class="card-img-top" alt="Digtal portrait of a person on a black and red background"">
+                        <div class="card-body">
+                            <p class="card-text">$ 0.00</p>
                         </div>
+                    </div>
+                    <div class="card pricing_imgBottom" style="height: 15rem; width: 18rem;">
+                        <img src="Assets/img/PricingSample_2.jpg" class="card-img-top" alt="Black and white print of a person on an abstract background">
+                        <div class="card-body">
+                            <p class="card-text">$ 0.00</p>
+                        </div>
+                    </div>
+                </div>
+                <div id = "sampleImages" class="col-4">
+                    <div class="card pricing_imgTop" style="height: 15rem; width: 18rem;">
+                        <img src="Assets/img/PricingSample_3.jpg"  class="card-img-top" alt="Digital portrait of a person in yellow on a black background">
+                        <div class="card-body">
+                            <p class="card-text">$ 0.00</p>
+                        </div>
+                    </div>
+                    <div class="card pricing_imgBottom" style="height: 15rem; width: 18rem;">
+                        <img src="Assets/img/PricingSample_4.png" class="card-img-top" alt="Digtal abstract art in black red and white">
+                        <div class="card-body">
+                            <p class="card-text">$ 0.00</p>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="col-8">
         </div>
         <?php include('footer.php'); ?>
         <script src="https://code.jquery.com/jquery.js"></script>
